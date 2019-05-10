748d1412a455c4d25c2b5587cacc5563a37342a7
Mikhail Shilkov <shilkov@gmail.com>
2019-05-10 17:56:03 +0200

    Inline assignments

--- examples/worker/index.ts
+++ examples/worker/index.ts
@@ -5,18 +5,15 @@ import * as fs from "fs";
 const config = new pulumi.Config();
 const zone =  config.require("zone");
 
-const pattern = `${zone}/helloworld`;
-const content = fs.readFileSync("worker.js", { encoding: "utf-8" });
-
 new cloudflare.WorkerScript("test", {
     zone,
-    content 
+    content: fs.readFileSync("worker.js", { encoding: 'utf-8' })
 });
 
 new cloudflare.WorkerRoute("test", {
-    enabled: true,
-    pattern,
     zone,
+    enabled: true,
+    pattern: `${zone}/helloworld`,
 });
 
-export const endpoint = `https://${pattern}`;
+export const endpoint = `https://${zone}/helloworld`;
