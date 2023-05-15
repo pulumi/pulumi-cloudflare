dd98293ff93643820b852dbff699098e0a0310b0
Anton Tayanovskyy <anton@pulumi.com>
2023-05-15 17:25:46 -0400

    Fix .NET test

--- examples/record/csharp/MyStack.cs
+++ examples/record/csharp/MyStack.cs
@@ -5,13 +5,15 @@ class MyStack : Stack
 {
     public MyStack()
     {
-        var config = new Pulumi.Config();
-        var zoneId = config.Require("zone_id");
+        var zone = new Cloudflare.Zone("my-zone", new Cloudflare.ZoneArgs
+        {
+            Name = "cs-test-cloudflare-pulumi.com"
+        });
 
         var foobar = new Cloudflare.Record("foobar", new Cloudflare.RecordArgs
         {
             Name = "my-record-csharp",
-            ZoneId = zoneId,
+            ZoneId = zone.ID,
             Value = "162.168.0.14",
             Type = "A",
             Ttl = 3600,
