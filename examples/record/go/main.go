9578a2448dda15954a40bb710d04f4131fb4e791
Anton Tayanovskyy <anton@pulumi.com>
2023-05-15 17:24:45 -0400

    Fix Go test

--- examples/record/go/main.go
+++ examples/record/go/main.go
@@ -3,17 +3,17 @@ package main
 import (
 	"github.com/pulumi/pulumi-cloudflare/sdk/v3/go/cloudflare"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
-	"github.com/pulumi/pulumi/sdk/v3/go/pulumi/config"
 )
 
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
-		c := config.New(ctx, "")
-		zoneId := c.Require("zone_id")
+		zone, err := cloudflare.NewZone(ctx, "my-zone", &cloudflare.ZoneArgs{
+			Zone: pulumi.String("go-test-cloudflare-pulumi.com"),
+		})
 
 		_, err := cloudflare.NewRecord(ctx, "my-record-go", &cloudflare.RecordArgs{
 			Name:   pulumi.String("my-record-go"),
-			ZoneId: pulumi.String(zoneId),
+			ZoneId: zone.ID(),
 			Type:   pulumi.String("A"),
 			Value:  pulumi.String("162.168.0.13"),
 			Ttl:    pulumi.Int(3600),
