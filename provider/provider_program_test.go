bbca44a6787f42bb3b7714c8ce9b865395ff4a87
Venelin <venelin@pulumi.com>
2024-04-03 12:58:27 +0100

    Generate tests

--- provider/provider_program_test.go
+++ provider/provider_program_test.go
@@ -5,9 +5,10 @@ import (
 	"path/filepath"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/pulumi/providertest/pulumitest"
 	"github.com/pulumi/providertest/pulumitest/opttest"
-	"github.com/stretchr/testify/require"
 )
 
 const providerName = "cloudflare"
