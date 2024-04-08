fbace56b55d549162e89c5a176e8ee01097d130e
Venelin <venelin@pulumi.com>
2024-04-08 19:07:42 +0100

    Tests

--- provider/provider_program_test.go
+++ provider/provider_program_test.go
@@ -1,20 +1,6 @@
 //go:build !go && !nodejs && !python && !dotnet
 // +build !go,!nodejs,!python,!dotnet
 
-// Copyright 2016-2024, Pulumi Corporation.
-//
-// Licensed under the Apache License, Version 2.0 (the "License")
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//	http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cloudflare
 
 import (
@@ -24,11 +10,84 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"github.com/pulumi/providertest"
+	"github.com/pulumi/providertest/optproviderupgrade"
 	"github.com/pulumi/providertest/pulumitest"
+	"github.com/pulumi/providertest/pulumitest/assertpreview"
 	"github.com/pulumi/providertest/pulumitest/opttest"
+	"github.com/pulumi/pulumi/sdk/v3/go/auto"
 )
 
 const providerName = "cloudflare"
+const defaultBaselineVersion = "5.25.0"
+
+var programs = []string{
+	"test-programs/index_workerscript",
+	"test-programs/.DS_Store",
+	"test-programs/index_workerskvnamespace",
+}
+
+func TestUpgradeCoverage(t *testing.T) {
+	providertest.ReportUpgradeCoverage(t)
+}
+
+type UpgradeTestOpts struct {
+	baselineVersion string
+	assertFunc      func(*testing.T, auto.PreviewResult)
+	config          map[string]string
+}
+
+func WithBaselineVersion(baselineVersion string) func(opts *UpgradeTestOpts) {
+	return func(opts *UpgradeTestOpts) {
+		opts.baselineVersion = baselineVersion
+	}
+}
+
+func WithAssertFunc(assertFunc func(*testing.T, auto.PreviewResult)) func(opts *UpgradeTestOpts) {
+	return func(opts *UpgradeTestOpts) {
+		opts.assertFunc = assertFunc
+	}
+}
+
+func WithConfig(config map[string]string) func(opts *UpgradeTestOpts) {
+	return func(opts *UpgradeTestOpts) {
+		opts.config = config
+	}
+}
+func testProviderUpgrade(t *testing.T, dir string, opts ...func(*UpgradeTestOpts)) {
+	options := &UpgradeTestOpts{}
+	for _, o := range opts {
+		o(options)
+	}
+	testProviderUpgradeWithOpts(t, dir, options.baselineVersion, options.config, options.assertFunc)
+}
+
+func testProviderUpgradeWithOpts(
+	t *testing.T, dir, baselineVersion string, config map[string]string,
+	assertFunction func(*testing.T, auto.PreviewResult),
+) {
+	if testing.Short() {
+		t.Skipf("Skipping in testing.Short() mode, assuming this is a CI run without credentials")
+	}
+	cwd, err := os.Getwd()
+	require.NoError(t, err)
+	if baselineVersion == "" {
+		baselineVersion = defaultBaselineVersion
+	}
+	test := pulumitest.NewPulumiTest(t, dir,
+		opttest.DownloadProviderVersion(providerName, baselineVersion),
+		opttest.LocalProviderPath(providerName, filepath.Join(cwd, "..", "bin")),
+	)
+	for k, v := range config {
+		test.SetConfig(k, v)
+	}
+	result := providertest.PreviewProviderUpgrade(test, providerName, baselineVersion, optproviderupgrade.DisableAttach())
+	if assertFunction != nil {
+		assertFunction(t, result)
+	} else {
+		assertpreview.HasNoReplacements(t, result)
+	}
+}
 
 func testProgram(t *testing.T, dir string) {
 	if testing.Short() {
@@ -38,27 +97,23 @@ func testProgram(t *testing.T, dir string) {
 	require.NoError(t, err)
 	test := pulumitest.NewPulumiTest(t, dir,
 		opttest.LocalProviderPath(providerName, filepath.Join(cwd, "..", "bin")),
-		opttest.SkipInstall(),
 	)
 	test.Up()
 }
 
 func TestPrograms(t *testing.T) {
-	programs := []string{
-		"test-programs/cloudflare_record",
-		"test-programs/cloudflare_zone",
-		"test-programs/cloudflare_zonesettingsoverride",
-		"test-programs/cloudflare_pagerule",
-		"test-programs/cloudflare_workerscript",
-		"test-programs/cloudflare_workerroute",
-		"test-programs/cloudflare_ruleset",
-		"test-programs/cloudflare_accessapplication",
-		"test-programs/cloudflare_pagesproject",
-		"test-programs/cloudflare_accesspolicy",
-	}
 	for _, p := range programs {
 		t.Run(p, func(t *testing.T) {
 			testProgram(t, p)
+		})
+	}
+}
+
+func TestProgramsUpgrade(t *testing.T) {
+	t.Skipf("skip upgrade tests for now as we have not recorded them.")
+	for _, p := range programs {
+		t.Run(p, func(t *testing.T) {
+			testProviderUpgrade(t, p)
 		})
 	}
 }
