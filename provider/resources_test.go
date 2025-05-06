f9eed562a4399a52d97abae73135edb955bedaef
Anton Tayanovskyy <anton@pulumi.com>
2025-05-06 10:42:55 -0400

    Reminder to revisit when schema version advances

--- provider/resources_test.go
+++ provider/resources_test.go
@@ -8,6 +8,7 @@ import (
 
 	shim "github.com/pulumi/pulumi-terraform-bridge/v3/pkg/tfshim"
 	"github.com/pulumi/pulumi/sdk/v3/go/common/util/contract"
+	"github.com/stretchr/testify/assert"
 )
 
 // TestSchemaVersionsForResetResources tests that the schema version of resources that were migrated
@@ -48,4 +49,13 @@ func TestSchemaVersionsForResetResources(t *testing.T) {
 		)
 		contract.Assertf(found, msg)
 	}
+}
+
+func TestRuleSetVersionReminder(t *testing.T) {
+	version.Version = "0.0.1"
+	p := Provider()
+	r := p.P.ResourcesMap().Get("cloudflare_ruleset")
+	assert.Equalf(t, 0, r.SchemaVersion(), "Reminder: cloudflare_ruleset advanced schema version from 0 and "+
+		"custom Pulumi PreStateUpgradeHook that massaged provider=v5 resource=v1 version data needs to be "+
+		"revisited or possibly dropped")
 }
