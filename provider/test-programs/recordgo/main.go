8af2fbe5c16b4f887f56849d5bdf0607584778ca
Bryce Lampe <bryce@pulumi.com>
2025-11-13 13:44:45 -0800

    lint

--- provider/test-programs/recordgo/main.go
+++ provider/test-programs/recordgo/main.go
@@ -9,14 +9,17 @@ import (
 func main() {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 		c := config.New(ctx, "")
-		accountId := c.Require("cloudflare-account-id")
+		accountID := c.Require("cloudflare-account-id")
 
 		zone, err := cloudflare.NewZone(ctx, "my-zone", &cloudflare.ZoneArgs{
-			Name:      pulumi.String("go-test-cloudflare-pulumi.com"),
+			Name: pulumi.String("go-test-cloudflare-pulumi.com"),
 			Account: cloudflare.ZoneAccountArgs{
-				Id: pulumi.String(accountId),
+				Id: pulumi.String(accountID),
 			},
 		})
+		if err != nil {
+			return err
+		}
 
 		_, err = cloudflare.NewRecord(ctx, "my-record-go", &cloudflare.RecordArgs{
 			Name:    pulumi.String("my-record-go.go-test-cloudflare-pulumi.com"),
